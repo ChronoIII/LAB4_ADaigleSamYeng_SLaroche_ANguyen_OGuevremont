c44f92343dd333cb6fd5b3f22e715867b9c06dd9
lexan02 <alex2@live.ca>
2015-04-06 16:33:04 -0400

    Heum

--- src/model/GestionnaireMemento.java
+++ src/model/GestionnaireMemento.java
@@ -1,5 +1,23 @@
 package model;
 
-public class GestionnaireMemento {
+import java.util.ArrayList;
 
+public class GestionnaireMemento<Param> {
+	private ArrayList<Memento<Param>> mementos = new ArrayList<Memento<Param>>();
+
+	public void addParam(Memento<Param> memento) {
+		mementos.add(memento);
+	}
+
+	
+	public Memento<Param> getParam(int index) {
+		return mementos.get(index);
+	}
+
+	/**
+	 * @return nombre de memento
+	 */
+	public int size() {
+		return mementos.size();
+	}
 }
--- src/model/Memento.java
+++ src/model/Memento.java
@@ -1,5 +1,13 @@
 package model;
 
-public class Memento {
+public class Memento<Param> {
+	private Param param;
 
+	public Memento(Param param) {
+		this.param = param;
+	}
+
+	public Param getPrecedent() {
+		return (this.param);
+	}
 }
