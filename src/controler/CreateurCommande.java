6a793353381b76add4ba0fa4ff47543bf10e294b
ChronoIII <samuel@jfzone.net>
2015-04-08 15:11:07 -0400

    bannière controle

--- src/controler/CreateurCommande.java
+++ src/controler/CreateurCommande.java
@@ -1,9 +1,22 @@
+/******************************************************
+Cours:   LOG121
+Session: H2015
+Groupe: 03
+Projet: Laboratoire # 4
+Étudiant(e)s: Samuel Laroche, Olivier Gévremont, Amélie Nguyen, Alexemdre Daigle-Sam yeng
+              
+              
+Chargé de cours : Francis Cardinal
+Chargé de laboratoire : Patrice Boucher
+Date créé: 2015-05-01
+*******************************************************
+Historique des modifications
+*******************************************************
+2015-05-01 Version initiale
+*******************************************************/ 
+
 package controler;
 
-/**
- * @author Alex
- *
- */
 public class CreateurCommande {
 
 	public CommandeZoom zoom(int aId, int aVariation) {
